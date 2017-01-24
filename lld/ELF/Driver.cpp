a41b946c21366269640ecf2c2bdafee9165b1b02
kettenis <kettenis@openbsd.org>
2017-01-24 10:21:53 +0000

    PIE-by-default.  Probably not suitable for upsttreaming in its current form.

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -986,7 +986,12 @@ static void readConfigs(opt::InputArgList &args) {
   config->optimize = args::getInteger(args, OPT_O, 1);
   config->orphanHandling = getOrphanHandling(args);
   config->outputFile = args.getLastArgValue(OPT_o);
+#ifdef __OpenBSD__
+  config->pie = args.hasFlag(OPT_pie, OPT_no_pie,
+      !args.hasArg(OPT_shared) && !args.hasArg(OPT_relocatable));
+#else
   config->pie = args.hasFlag(OPT_pie, OPT_no_pie, false);
+#endif
   config->printIcfSections =
       args.hasFlag(OPT_print_icf_sections, OPT_no_print_icf_sections, false);
   config->printGcSections =
