279ee85df411d450e3c999b976857fdaac2ef839
robert <robert@openbsd.org>
2017-07-27 20:15:24 +0000

    teach clang about our syslog format attribute
    ok deraadt@

--- clang/include/clang/Sema/Sema.h
+++ clang/include/clang/Sema/Sema.h
@@ -11769,6 +11769,7 @@ public:
     FST_FreeBSDKPrintf,
     FST_OSTrace,
     FST_OSLog,
+    FST_Syslog,
     FST_Unknown
   };
   static FormatStringType GetFormatStringType(const FormatAttr *Format);
