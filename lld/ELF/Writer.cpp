99e8388ec98de0630e30cd5f1aa391cf9f0ce73c
kettenis <kettenis@openbsd.org>
2018-10-24 21:19:03 +0000

    Generate __data_start symbol that marks the start of .data when __data_start is referenced from code being linked.
    ok deraadt@, naddy@, guenther@

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -320,6 +320,7 @@ void elf::addReservedSymbols() {
   };
 
   ElfSym::bss = add("__bss_start", 0);
+  ElfSym::data = Add("__data_start", 0);
   ElfSym::end1 = add("end", -1);
   ElfSym::end2 = add("_end", -1);
   ElfSym::etext1 = add("etext", -1);
@@ -1184,6 +1185,9 @@ template <class ELFT> void Writer<ELFT>::setReservedSymbolSections() {
 
   if (ElfSym::bss)
     ElfSym::bss->section = findSection(".bss");
+
+  if (ElfSym::data)
+    ElfSym::data->section = findSection(".data");
 
   // Setup MIPS _gp_disp/__gnu_local_gp symbols which should
   // be equal to the _gp symbol's value.
