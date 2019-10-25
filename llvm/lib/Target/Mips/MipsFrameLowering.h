5f6386c7ece07f676213cff76b1eb38e51735986
mortimer <mortimer@openbsd.org>
2019-10-25 00:40:56 +0000

    Add retguard for octeon/mips64.
    For this architecture we use separate retguard prologue and epilogue code
    for static or PIC code. In the PIC case we use some additional code before
    the retguard epilogue to recover the function start address and the GOT
    pointer in order to get the per-function random cookie. Much thanks to
    visa@ for suggestions and advice making it all work.
    
    ok deraadt@ visa@

--- llvm/lib/Target/Mips/MipsFrameLowering.h
+++ llvm/lib/Target/Mips/MipsFrameLowering.h
@@ -14,6 +14,7 @@
 #define LLVM_LIB_TARGET_MIPS_MIPSFRAMELOWERING_H
 
 #include "Mips.h"
+#include "MipsReturnProtectorLowering.h"
 #include "llvm/CodeGen/TargetFrameLowering.h"
 
 namespace llvm {
@@ -24,8 +25,10 @@ protected:
   const MipsSubtarget &STI;
 
 public:
+  const MipsReturnProtectorLowering RPL;
+
   explicit MipsFrameLowering(const MipsSubtarget &sti, Align Alignment)
-      : TargetFrameLowering(StackGrowsDown, Alignment, 0, Alignment), STI(sti) {
+      : TargetFrameLowering(StackGrowsDown, Alignment, 0, Alignment), STI(sti), RPL() {
   }
 
   static const MipsFrameLowering *create(const MipsSubtarget &ST);
@@ -39,6 +42,8 @@ public:
   bool enableShrinkWrapping(const MachineFunction &MF) const override {
     return true;
   }
+
+  const ReturnProtectorLowering *getReturnProtector() const override;
 
   MachineBasicBlock::iterator
   eliminateCallFramePseudoInstr(MachineFunction &MF,
